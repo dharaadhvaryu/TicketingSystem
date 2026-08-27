01314148df334b3ca221957fd7a76eaec8a41b4a
dharaadhvaryu <dharaadhvaryu@gmail.com>
2026-08-27 10:14:05 +0100

    Add new changes

---
+++ index/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+#from django.shortcuts import render
+
+def index(request):
+    return render(
+            request,
+            'index/index.html'
+           
+        )
+    
+
+# Create your views here.
